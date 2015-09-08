a2162b9ca9ebea5e3f6949bfdbc373fd6476ddf8
Matt Insler <matt.insler@gmail.com>
2015-09-08 03:46:31 -0400

    updates for connie 0.0.3 and app-context 0.1.x

--- example.js
+++ example.js
@@ -1,16 +1,3 @@
-# app-context-connie
-
-[connie](https://www.npmjs.com/package/connie) initializer for [app-context](https://www.npmjs.com/package/app-context)
-
-### Installation
-
-```bash
-$ npm install --save app-context-connie
-```
-
-### Usage
-
-```javascript
 module.exports = function() {
   this.runlevel('configured')
     // load configuration from a JSON file
@@ -20,4 +7,3 @@ module.exports = function() {
     // load configuration from a JSON HTTP endpoint
     .use('connie', 'http', 'http://user:pass@my-config-server.com/config.json');
 };
-```
